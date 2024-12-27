23a775df63e3e5bd0a678454fc4fd1350f96951a
unknown <ngeno.gilbert@nlske.com>
2024-12-27 17:37:18 +0300

    Add UI login and register

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -14,6 +14,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import Link from "next/link";
 import React from "react";
 import theme from "./theme";
+import AppHeader from "./AppHeader";
 
 export default function RootLayout({
   children,
